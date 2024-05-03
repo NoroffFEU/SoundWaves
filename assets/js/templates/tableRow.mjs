513bcb1407c7d5584d1ac900f7ff364e259c5e75
Jesús Alberola Herrero <jesusalberola90@gmail.com>
2024-05-03 10:19:47 +0200

    Changed dates

--- assets/js/templates/tableRow.mjs
+++ assets/js/templates/tableRow.mjs
@@ -1,4 +1,9 @@
 export const tableRowTemplate = (data) => {
+    const date = new Date(data.created);
+    const day = date.getUTCDate().toString().padStart(2, "0");
+    const month = (date.getUTCMonth() + 1).toString().padStart(2, "0");
+    const year = date.getUTCFullYear();
+    data.created = `${day}/${month}/${year}`;
   return `
     <tr class="responsive-tr">
         <td data-cell="title">${data.title}</td>
@@ -16,24 +21,10 @@ export const tableRowTemplate = (data) => {
     `;
 };
 
+
+//TODO: Add error template
+
 export const tableRowTemplateError = (error) => {
     return `
             <td data-cell="title">${error}</td>
     `; }
-
-// export const tableTemplate = `
-//     <tr class="responsive-tr">
-//         <td data-cell="title">{{title}}</td>
-//         <td data-cell="category"></td>
-//         <td data-cell="date"></td>
-//         <td data-cell="actions" class="flex center align-items-center gap-30">
-//             <button type="button" aria-label="Edit" class="edit-btn">
-//                 <i class="fa-solid fa-pen"></i>
-//             </button>
-//             <button type="button" aria-label="Delete" class="delete-btn">
-//                 <i class="fa-solid fa-trash"></i>
-//             </button>
-//         </td>
-//     </tr>
-
-// `;
