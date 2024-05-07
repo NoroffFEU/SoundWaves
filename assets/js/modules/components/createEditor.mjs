47220cf78b24e9b8f0781f69ea8abf89e81459ab
Jesús Alberola Herrero <jesusalberola90@gmail.com>
2024-05-07 09:22:15 +0200

    finishing create post

--- assets/js/modules/components/createEditor.mjs
+++ assets/js/modules/components/createEditor.mjs
@@ -2,15 +2,61 @@ import { BASE_URL, URLs } from "../../utils/constants.mjs";
 import { createPost } from "../api/blog/createPost.mjs";
 
 
-const thumbnailBackground = document.querySelector('.thumbnail-background');
 const thumbnailInput = document.querySelector('#thumbnail');
-const thumbnailButton = document.querySelector('.thumbnail-background span');
-
-
-
-
 const CLIENT_ID = 'd7ac36c85a3852a'; 
 const ACCESS_TOKEN = '677864924d901ab7d356d7e28497937ca088e659'; 
+
+
+thumbnailInput.addEventListener('change', async (event) => {
+  const thumbnailBackground = document.querySelector('.thumbnail-background');
+  const thumbnailButton = document.querySelector('.thumbnail-background span');
+  const imageUrl = document.querySelector('.image-url');
+
+  try {
+    const response = await fetch('https://api.imgur.com/3/image', {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${ACCESS_TOKEN}`
+      },
+      body: event.target.files[0],
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      thumbnailBackground.style.backgroundImage = `url(${data.data.link})`;
+      imageUrl.textContent = data.data.link;
+      
+      if (thumbnailBackground.style.backgroundImage) {
+        thumbnailButton.textContent = 'Change thumbnail';
+      }
+    } else {
+      console.error('Error uploading image to Imgur:', data);
+    }
+
+  } catch (error) {
+    console.error('Error uploading image to Imgur:', error);
+  }
+});
+
+
+
+const createPostForm = document.forms.createForm;
+
+createPostForm.addEventListener('submit', async (event)=> {
+    event.preventDefault();
+    const imageUrl = document.querySelector('.image-url').textContent;
+    const content  = tinymce.activeEditor.getContent();
+    const title = createPostForm.title.value;
+    const category = createPostForm.category.value;
+    const media = { url : imageUrl}
+    
+    await createPost('Jesus_AH', title, content, category, media)
+
+    window.location.href = `${BASE_URL}${URLs.adminPanel}`;
+})
+
+
 
 // thumbnailInput.addEventListener('change', (event) => {
   
@@ -38,55 +84,6 @@ const ACCESS_TOKEN = '677864924d901ab7d356d7e28497937ca088e659';
 //     console.error('Error uploading image to Imgur:', error);
 //   });
 // });
-
-thumbnailInput.addEventListener('change', async (event) => {
-  try {
-    const response = await fetch('https://api.imgur.com/3/image', {
-      method: 'POST',
-      headers: {
-        Authorization: `Bearer ${ACCESS_TOKEN}`
-      },
-      body: event.target.files[0],
-    });
-
-    const data = await response.json();
-
-    if (data.success) {
-      thumbnailBackground.style.backgroundImage = `url(${data.data.link})`;
-      
-      if (thumbnailBackground.style.backgroundImage) {
-        thumbnailButton.textContent = 'Change thumbnail';
-      }
-    } else {
-      console.error('Error uploading image to Imgur:', data);
-    }
-  } catch (error) {
-    console.error('Error uploading image to Imgur:', error);
-  }
-});
-
-
-
-const formulario = document.forms.createForm;
-
-formulario.addEventListener('submit', async (event)=> {
-    event.preventDefault();
-    const imgUrl = document.querySelector('#url').textContent;
-    const content  = tinymce.activeEditor.getContent();
-    const title = formulario.title.value;
-    const category = formulario.category.value;
-    const thumbnail = imgUrl;
-    
-    const media = { url : thumbnail}
-    
-   await createPost('Jesus_AH', title, content, category, media)
-
-   window.location.href = `${BASE_URL}${URLs.adminPanel}`;
-
-})
-
-
-
 
 
 
