6053ea9b42305bd87720bdc03526ff1e35f527ea
Jesús Alberola Herrero <jesusalberola90@gmail.com>
2024-05-08 12:56:49 +0200

    fixed issue with interval in the carousel

--- assets/js/modules/components/carousel.mjs
+++ assets/js/modules/components/carousel.mjs
@@ -1,63 +1,72 @@
 import { getPostsByUser } from "../api/blog/getAllPosts.mjs";
 
+let intervalId;
+
 export function loadCarousel() {
-  const prevButton = document.querySelector(".prev-slide");
-  const nextButton = document.querySelector(".next-slide");
-  const container = document.querySelector(".hero article")
+    const prevButton = document.querySelector(".prev-slide");
+    const nextButton = document.querySelector(".next-slide");
 
-  prevButton.addEventListener("click", handlePrevSlide);
-  nextButton.addEventListener("click", handleNextSlide);
+    prevButton.addEventListener("click", (e) => {
+        handlePrevSlide();
+        restartInterval();
+    });
 
-  // set interval to handle next slide automatically
-  setInterval(() => {
-    handleNextSlide();
-  }, 5000);
+    nextButton.addEventListener("click", (e) => {
+        handleNextSlide();
+        restartInterval();
+    });
 
+    startInterval();
+}
+
+function startInterval() {
+    intervalId = setInterval(handleNextSlide, 2000);
+}
+
+function restartInterval() {
+    clearInterval(intervalId);
+    startInterval();
 }
 
 function handleNextSlide() {
-    const container = document.querySelector(".hero article")
+    const container = document.querySelector(".hero article");
     const firstSlide = document.querySelector("[data-position='first']");
     const secondSlide = document.querySelector("[data-position='second']");
     const thirdSlide = document.querySelector("[data-position='third']");
-    
-    if (firstSlide.dataset.position === 'first' && firstSlide.dataset.active === "true") {
+
+    if (firstSlide.dataset.position === "first" && firstSlide.dataset.active === "true") {
         firstSlide.dataset.active = "false";
         secondSlide.dataset.active = "true";
-        container.style.transform = 'translateX(-100%)';
-    }
-    else if (secondSlide.dataset.position === 'second' && secondSlide.dataset.active === "true") {
+        container.style.transform = "translateX(-100%)";
+    } else if (secondSlide.dataset.position === "second" && secondSlide.dataset.active === "true") {
         secondSlide.dataset.active = "false";
         thirdSlide.dataset.active = "true";
-        container.style.transform = 'translateX(-200%)';
-    }
-    else if (thirdSlide.dataset.position === 'third' && thirdSlide.dataset.active === "true") {
+        container.style.transform = "translateX(-200%)";
+    } else if (thirdSlide.dataset.position === "third" && thirdSlide.dataset.active === "true") {
         thirdSlide.dataset.active = "false";
         firstSlide.dataset.active = "true";
-        container.style.transform = 'translateX(0%)';
+        container.style.transform = "translateX(0%)";
     }
 }
 
 function handlePrevSlide() {
-    const container = document.querySelector(".hero article")
+    const container = document.querySelector(".hero article");
     const firstSlide = document.querySelector("[data-position='first']");
     const secondSlide = document.querySelector("[data-position='second']");
     const thirdSlide = document.querySelector("[data-position='third']");
-    
-    if (firstSlide.dataset.position === 'first' && firstSlide.dataset.active === "true") {
+
+    if (firstSlide.dataset.position === "first" && firstSlide.dataset.active === "true") {
         firstSlide.dataset.active = "false";
         thirdSlide.dataset.active = "true";
-        container.style.transform = 'translateX(-200%)';
-    }
-    else if (secondSlide.dataset.position === 'second' && secondSlide.dataset.active === "true") {
+        container.style.transform = "translateX(-200%)";
+    } else if (secondSlide.dataset.position === "second" && secondSlide.dataset.active === "true") {
         secondSlide.dataset.active = "false";
         firstSlide.dataset.active = "true";
-        container.style.transform = 'translateX(0%)';
-    }
-    else if (thirdSlide.dataset.position === 'third' && thirdSlide.dataset.active === "true") {
+        container.style.transform = "translateX(0%)";
+    } else if (thirdSlide.dataset.position === "third" && thirdSlide.dataset.active === "true") {
         thirdSlide.dataset.active = "false";
         secondSlide.dataset.active = "true";
-        container.style.transform = 'translateX(-100%)';
+        container.style.transform = "translateX(-100%)";
     }
 }
 
