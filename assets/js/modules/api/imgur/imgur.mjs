04a8f46bc9b6cd1aac2d4dd93bef0bffc5118a12
Jesús Alberola Herrero <jesusalberola90@gmail.com>
2024-05-17 07:29:44 +0200

    try fix imgur

--- assets/js/modules/api/imgur/imgur.mjs
+++ assets/js/modules/api/imgur/imgur.mjs
@@ -1,19 +1,22 @@
 export async function uploadImageToImgur(imageFile) {
-  const ACCESS_TOKEN = "677864924d901ab7d356d7e28497937ca088e659"; 
+  const ACCESS_TOKEN = "677864924d901ab7d356d7e28497937ca088e659";
 
   try {
+    const formData = new FormData();
+    formData.append('image', imageFile);
+
     const response = await fetch("https://api.imgur.com/3/image", {
       method: "POST",
       headers: {
         Authorization: `Bearer ${ACCESS_TOKEN}`,
       },
-      body: imageFile,
+      body: formData,
     });
 
     const data = await response.json();
 
     if (data.success) {
-      return data.data.link; 
+      return data.data.link;
     } else {
       console.error("Error uploading image to Imgur:", data);
       return null;
