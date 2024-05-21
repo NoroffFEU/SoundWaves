2ad1293f222952c74febd10ecaf75b38a06067a3
Jesús Alberola Herrero <jesusalberola90@gmail.com>
2024-05-21 10:20:20 +0200

    Cleaning code

--- assets/js/pages/createPage.mjs
+++ assets/js/pages/createPage.mjs
@@ -2,58 +2,8 @@ import { loginUser } from "../modules/api/auth/login.mjs";
 import { createPost } from "../modules/api/blog/createPost.mjs";
 import { BASE_URL, URLs } from "../utils/constants.mjs";
 import { getTokenFromLocalStorage, getUserFromLocalStorage } from "../utils/getLocalStorages.mjs";
-import { uploadImageToImgur } from "../modules/api/imgur/imgur.mjs";
 import { loadDiscardButton } from "../modules/components/discardBtn.mjs";
 import { handleThumbnailUpload } from "../utils/handleThumbnailUpload.mjs";
-
-// Thumbnail and IMGur upload
-// const thumbnailInput = document.querySelector("#thumbnail");
-// thumbnailInput.addEventListener("change", async (event) => {
-//   const thumbnailBackground = document.querySelector(".thumbnail-background");
-//   const thumbnailButton = document.querySelector(".thumbnail-background span");
-//   const imageUrl = document.querySelector(".image-url");
-
-//   try {
-//     const uploadedImageUrl = await uploadImageToImgur(event.target.files[0]);
-  
-//     if (uploadedImageUrl) {
-//       thumbnailBackground.style.backgroundImage = `url(${uploadedImageUrl})`;
-//       imageUrl.textContent = uploadedImageUrl;
-//       thumbnailButton.textContent = "Change thumbnail";
-//     } else {
-//       alert("Error uploading image to Imgur, please try again later.")
-//     }
-//   } catch (error) {
-//     console.error("Error uploading image to Imgur:", error);
-//   }
-// });
-
-// const thumbnailInput = document.querySelector("#thumbnail");
-
-// thumbnailInput.addEventListener("change", async (event) => {
-//   const thumbnailBackground = document.querySelector(".thumbnail-background");
-//   const thumbnailButton = document.querySelector(".thumbnail-background span");
-//   const imageUrl = document.querySelector(".image-url");
-//   const file = event.target.files[0];
-
-//   if (file && (file.type === "image/jpeg" || file.type === "image/jpg")) {
-//     try {
-//       const uploadedImageUrl = await uploadImageToImgur(file);
-    
-//       if (uploadedImageUrl) {
-//         thumbnailBackground.style.backgroundImage = `url(${uploadedImageUrl})`;
-//         imageUrl.textContent = uploadedImageUrl;
-//         thumbnailButton.textContent = "Change thumbnail";
-//       } else {
-//         alert("Error uploading image to Imgur, please try again later.");
-//       }
-//     } catch (error) {
-//       console.error("Error uploading image to Imgur:", error);
-//     }
-//   } else {
-//     alert("Please upload a JPEG image.");
-//   }
-// });
 
 // Create post form
 const createPostForm = document.forms.createForm;
