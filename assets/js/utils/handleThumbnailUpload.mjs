f70b79ac72c74de5a54661ac3e83096ec6e586a7
Jesús Alberola Herrero <jesusalberola90@gmail.com>
2024-05-22 11:05:27 +0200

    try refresh token

--- assets/js/utils/handleThumbnailUpload.mjs
+++ assets/js/utils/handleThumbnailUpload.mjs
@@ -1,6 +1,7 @@
 const thumbnailInput = document.querySelector('#thumbnail');
 const ACCESS_TOKEN = '677864924d901ab7d356d7e28497937ca088e659';
 const CLIENT_ID = 'd7ac36c85a3852a'; 
+const REFRESH_TOKEN = '801a424005032b6dd9cd2d8d6df2c2ce63a272aa';
 
 export async function handleThumbnailUpload() {
   thumbnailInput.addEventListener('change', async (event) => {
@@ -18,10 +19,12 @@ export async function handleThumbnailUpload() {
     }
 
     try {
+      let accessToken = await getAccessToken();
+
       const response = await fetch('https://api.imgur.com/3/image', {
         method: 'POST',
         headers: {
-          Authorization: `Bearer ${ACCESS_TOKEN}`
+          Authorization: `Bearer ${accessToken}`
         },
         body: file,
       });
@@ -43,4 +46,24 @@ export async function handleThumbnailUpload() {
       console.error('Error uploading image to Imgur:', error);
     }
   });
+}
+
+async function getAccessToken() {
+  const response = await fetch('https://api.imgur.com/oauth2/token', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      refresh_token: REFRESH_TOKEN,
+      client_id: CLIENT_ID,
+      client_secret: '7c656c7e69236a558d27b666225ed536b4831e05',
+      grant_type: 'refresh_token',
+    }),
+  });
+
+  const data = await response.json();
+  console.log(data.access_token)
+
+  return data.access_token;
 }
