e3dffd2c0553fe639dbd2b8d21d69c8d53cf62c0
Jesús Alberola Herrero <jesusalberola90@gmail.com>
2024-05-22 06:56:50 +0200

    reduce code in delete button, also added confirm to delete

--- assets/js/modules/components/deleteBtn.mjs
+++ assets/js/modules/components/deleteBtn.mjs
@@ -10,19 +10,16 @@ export function loadDeleteBtn() {
 
 async function removePost(event) {
     const dataID = event.target.getAttribute("data-post-id");
+
+    const confirmDelete = confirm("Are you sure you want to delete this post?")
+    if (!confirmDelete) {
+      return;
+    }
+
     try {
-      if(localStorage.getItem('userData')) {
-        const storedUser = localStorage.getItem('userData');
-        const userData = JSON.parse(storedUser);
-        const name = userData.name;
-        const token = userData.token;
-        const deletedPost = await deletePost(dataID, name, token);
-        await loadFivePosts();
-      } else {
-        const adminToken = await loginUser("jesalb53435@stud.noroff.no", "IamTheAdmin");
-        const deletedPost = await deletePost(dataID, 'Jesus_AH', adminToken);
-        await loadFivePosts();
-      }
+      const userData = localStorage.getItem('userData') ? JSON.parse(localStorage.getItem('userData')) : { name: 'Jesus_AH', token: await loginUser("jesalb53435@stud.noroff.no", "IamTheAdmin") };
+      await deletePost(dataID, userData.name, userData.token);
+      await loadFivePosts();
     } catch (error) {
         console.error(error);
     } 
