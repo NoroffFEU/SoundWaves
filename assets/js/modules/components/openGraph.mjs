f4a0749db9d51018e265707b82362d2582fe4b16
Jesús Alberola Herrero <jesusalberola90@gmail.com>
2024-05-24 07:03:26 +0200

    Indent and change OG

--- assets/js/modules/components/openGraph.mjs
+++ assets/js/modules/components/openGraph.mjs
@@ -1,18 +1,36 @@
 import { BASE_URL, URLs } from "../../utils/constants.mjs";
 
 export function updateOpenGraph(post) {
-    updateFacebookOG(post);
-    updateTwitterOG(post);
+  updateFacebookOG(post);
+  updateTwitterOG(post);
 }
 
 function updateFacebookOG(post) {
-    document.querySelector('meta[property="og:title"]').setAttribute("content", `${post.title}`);
-    document.querySelector('meta[property="og:description"]').setAttribute("content", `${post.body}`);
-    document.querySelector('meta[property="og:url"]').setAttribute("content", `${BASE_URL}${URLs.post}?id=${post.id}`);
+  document
+    .querySelector('meta[property="og:title"]')
+    .setAttribute("content", `${post.title}`);
+  document
+    .querySelector('meta[property="og:description"]')
+    .setAttribute(
+      "content",
+      `Discover more about ${post.title} on SoundWaves Discoveries!`
+    );
+  document
+    .querySelector('meta[property="og:url"]')
+    .setAttribute("content", `${BASE_URL}${URLs.post}?id=${post.id}`);
 }
 
 function updateTwitterOG(post) {
-    document.querySelector('meta[name="twitter:title"]').setAttribute("content", `${post.title}`);
-    document.querySelector('meta[name="twitter:description"]').setAttribute("content", `${post.body}`);
-    document.querySelector('meta[property="twitter:url"]').setAttribute("content",  `${BASE_URL}${URLs.post}?id=${post.id}`);
+  document
+    .querySelector('meta[name="twitter:title"]')
+    .setAttribute("content", `${post.title}`);
+  document
+    .querySelector('meta[name="twitter:description"]')
+    .setAttribute(
+      "content",
+      `Discover more about ${post.title} on SoundWaves Discoveries!`
+    );
+  document
+    .querySelector('meta[property="twitter:url"]')
+    .setAttribute("content", `${BASE_URL}${URLs.post}?id=${post.id}`);
 }
