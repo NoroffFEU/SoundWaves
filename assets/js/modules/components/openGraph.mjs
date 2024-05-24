a0fdbeb0c6c4637726baf4e76c443360fbe9b322
Jesús Alberola Herrero <jesusalberola90@gmail.com>
2024-05-24 07:01:27 +0200

    Leave the logo in the opengraph

--- assets/js/modules/components/openGraph.mjs
+++ assets/js/modules/components/openGraph.mjs
@@ -8,13 +8,11 @@ export function updateOpenGraph(post) {
 function updateFacebookOG(post) {
     document.querySelector('meta[property="og:title"]').setAttribute("content", `${post.title}`);
     document.querySelector('meta[property="og:description"]').setAttribute("content", `${post.body}`);
-    document.querySelector('meta[property="og:image"]').setAttribute("content", `${post.media.url}`);
     document.querySelector('meta[property="og:url"]').setAttribute("content", `${BASE_URL}${URLs.post}?id=${post.id}`);
 }
 
 function updateTwitterOG(post) {
     document.querySelector('meta[name="twitter:title"]').setAttribute("content", `${post.title}`);
     document.querySelector('meta[name="twitter:description"]').setAttribute("content", `${post.body}`);
-    document.querySelector('meta[name="twitter:image"]').setAttribute("content", `${post.media.url}`);
     document.querySelector('meta[property="twitter:url"]').setAttribute("content",  `${BASE_URL}${URLs.post}?id=${post.id}`);
 }
